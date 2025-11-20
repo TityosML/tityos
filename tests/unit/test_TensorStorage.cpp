cb859a8bdac2bbe3edfb7d862b005d2c279e1ebd
Oscar Milligan <milliganoscar2007@gmail.com>
2025-11-20 12:09:43 +0000

    Made tests more descriptive

--- tests/unit/test_TensorStorage.cpp
+++ tests/unit/test_TensorStorage.cpp
@@ -6,10 +6,12 @@ TEST_CASE("TensorStorage can be allocated to CPU", "[TensorStorage]") {
     REQUIRE_NOTHROW(ty::internal::TensorStorage(4 * sizeof(int)));
 }
 
-TEST_CASE("TensorStorage can be allocated to GPU", "[TensorStorage]") {
-    #ifdef TITYOS_USE_CUDA
+#ifdef TITYOS_USE_CUDA
+    TEST_CASE("TensorStorage can be allocated to GPU with CUDA", "[TensorStorage]") {
         REQUIRE_NOTHROW(ty::internal::TensorStorage(4 * sizeof(int), {ty::DeviceType::CUDA, 0}));
-    #else
+    }
+#else
+    TEST_CASE("TensorStorage fails to be allocated to CUDA when CUDA not used", "[TensorStorage]") {
         REQUIRE_THROWS(ty::internal::TensorStorage(4 * sizeof(int), {ty::DeviceType::CUDA, 0}));
-    #endif
-}
+    }
+#endif

7313c53b5d9f4fcd8b5e0af22984dacd40e47e5a
Dylan-Austin <dylanaustinrhys@gmail.com>
2025-11-24 19:25:14 +0000

    changed Acessing Tensor test to test indexing using vectors and arrays

--- tests/unit/test_Tensor.cpp
+++ tests/unit/test_Tensor.cpp
@@ -20,7 +20,7 @@ TEST_CASE("Accessing Tensor", "[Tensor]") {
     ty::Tensor example(std::vector<float>({1.0, 2.0, 3.0, 4.0}), {2, 2});
 
     CHECK(*(float*)example.at({0, 0}) == 1.0);
-    CHECK(*(float*)example.at({0, 1}) == 2.0);
-    CHECK(*(float*)example.at({1, 0}) == 3.0);
+    CHECK(*(float*)example.at(std::vector<size_t>({0, 1})) == 2.0);
+    CHECK(*(float*)example.at(std::array<size_t, 2>({1, 0})) == 3.0);
     CHECK(*(float*)example.at({1, 1}) == 4.0);
 }

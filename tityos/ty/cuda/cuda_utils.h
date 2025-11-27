916f345d7d55dc98cee5d0a01bb2ae5b38eb336a
Dylan-Austin <dylanaustinrhys@gmail.com>
2025-11-27 17:56:10 +0000

    reformatted cuda_utils.h

--- tityos/ty/cuda/cuda_utils.h
+++ tityos/ty/cuda/cuda_utils.h
@@ -3,14 +3,14 @@
 #include "tityos/ty/cuda/cuda_import.h"
 
 namespace ty {
-    namespace cuda {
-        inline bool isCudaAvailable() {
-            #ifdef TITYOS_USE_CUDA
-                // TODO: Change this to check for device availablilty
-                return true;
-            #else
-                return false;
-            #endif
-        }
-    }
+namespace cuda {
+inline bool isCudaAvailable() {
+#ifdef TITYOS_USE_CUDA
+    // TODO: Change this to check for device availablilty
+    return true;
+#else
+    return false;
+#endif
 }
+} // namespace cuda
+} // namespace ty

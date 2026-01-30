48d9af07ade3320f29803d1eed9c853996347254
SJ1727 <samueljohnson1727@gmail.com>
2026-01-30 14:41:36 +0000

    Added more accumulators for bmm avx2

--- tityos/ty/ops/cpu/avx2/bmm.cpp
+++ tityos/ty/ops/cpu/avx2/bmm.cpp
@@ -8,6 +8,7 @@ namespace internal {
                        const TensorView<T>& batch2View) {
         using Vec = typename Avx2Traits<T>::Vec;
         constexpr int lanes = Avx2Traits<T>::lanes;
+        constexpr size_t NACC = 4;
 
         const size_t B = outView.shape[0];
         const size_t M = outView.shape[1];
@@ -19,41 +20,58 @@ namespace internal {
         const T* batch1Data = batch1View.data + batch1View.offset;
         const T* batch2Data = batch2View.data + batch2View.offset;
 
-        std::memset(outData, 0, total * sizeof(T));
-
-#pragma omp parallel for
+#pragma omp parallel for collapse(2) schedule(static)
         for (size_t batch = 0; batch < B; batch++) {
             for (size_t m = 0; m < M; m++) {
-                for (size_t n = 0; n < N; n++) {
+
+                size_t k = 0;
+                for (; k + NACC * lanes <= K; k += NACC * lanes) {
+                    size_t outIdx1 = batch * outView.strides[0] +
+                                     m * outView.strides[1] +
+                                     k * outView.strides[2];
                     size_t batch1Idx = batch * batch1View.strides[0] +
-                                       m * batch1View.strides[1] +
-                                       n * batch1View.strides[2];
+                                       m * batch1View.strides[1];
+                    size_t batch2Idx = batch * batch2View.strides[0] +
+                                       k * batch2View.strides[2];
 
-                    Vec vecBatch1 = Avx2Traits<T>::set1(batch1Data[batch1Idx]);
+                    Vec outVecs[NACC];
+                    Vec batch2Vecs[NACC];
 
-                    size_t k = 0;
-                    for (; k + lanes <= K; k += lanes) {
-                        size_t outIdx = batch * outView.strides[0] +
-                                        m * outView.strides[1] +
-                                        k * outView.strides[2];
-                        size_t batch2Idx = batch * batch2View.strides[0] +
-                                           n * batch2View.strides[1] +
-                                           k * batch2View.strides[2];
-
-                        Vec vecOut = Avx2Traits<T>::load(outData + outIdx);
-                        Vec vecBatch2 =
-                            Avx2Traits<T>::load(batch2Data + batch2Idx);
-
-                        vecOut =
-                            Avx2Traits<T>::fma(vecBatch1, vecBatch2, vecOut);
-
-                        Avx2Traits<T>::store(outData + outIdx, vecOut);
+                    for (size_t i = 0; i < NACC; i++) {
+                        outVecs[i] = Avx2Traits<T>::empty();
                     }
 
-                    for (; k < K; k++) {
-                        size_t outIdx = batch * outView.strides[0] +
-                                        m * outView.strides[1] +
-                                        k * outView.strides[2];
+                    for (size_t n = 0; n < N; n++) {
+                        Vec batch1Vec =
+                            Avx2Traits<T>::set1(batch1Data[batch1Idx]);
+
+                        for (size_t i = 0; i < NACC; i++) {
+                            batch2Vecs[i] = Avx2Traits<T>::load(
+                                batch2Data + batch2Idx + i * lanes);
+                            outVecs[i] = Avx2Traits<T>::fma(
+                                batch1Vec, batch2Vecs[i], outVecs[i]);
+                        }
+
+                        batch1Idx += batch1View.strides[2];
+                        batch2Idx += batch2View.strides[1];
+                    }
+
+                    for (size_t i = 0; i < NACC; i++) {
+                        Avx2Traits<T>::store(outData + outIdx1 + i * lanes,
+                                             outVecs[i]);
+                    }
+                }
+
+                for (; k < K; k++) {
+                    size_t outIdx = batch * outView.strides[0] +
+                                    m * outView.strides[1] +
+                                    k * outView.strides[2];
+                    outData[outIdx] = 0;
+
+                    for (size_t n = 0; n < N; n++) {
+                        size_t batch1Idx = batch * batch1View.strides[0] +
+                                           m * batch1View.strides[1] +
+                                           n * batch1View.strides[2];
                         size_t batch2Idx = batch * batch2View.strides[0] +
                                            n * batch2View.strides[1] +
                                            k * batch2View.strides[2];
